cd6e98efa5688858dbdab6c52026ad3f5b925ca1
Sofía Hernández <sofiachp95@gmail.com>
2023-10-11 21:14:27 -0300

    Offcanvas funcionando

--- js/init.js
+++ js/init.js
@@ -23,45 +23,47 @@ let getJSONData = function(url){
     });
 }
 
-let currentProductsArray = [];
+let listaDePeliculas = [];
 
 document.addEventListener("DOMContentLoaded", function(e){
   getJSONData(url).then(function(resultObj){
       if (resultObj.status === "ok"){
-          currentProductsArray = resultObj.data;
-          console.log(currentProductsArray);
+        listaDePeliculas = resultObj.data;
+          console.log(listaDePeliculas);
           }
           //Buscador
           document.getElementById("btnBuscar").addEventListener('click', () => {
             listContainer.innerHTML = "" //Vacia la lista
             let searchTerm = document.getElementById("inputBuscar").value.toUpperCase();
             console.log(searchTerm)
-            showProductList(searchTerm);
+            mostrarDetallesPelicula(searchTerm);
           }); // Aqui termina el evento del boton buscar
 
           //funcion para mostrar los resultados de la busqueda
-          function showProductList(searchTerm){
-            currentProductsArray.forEach((p, index) => {
+          function mostrarDetallesPelicula(searchTerm){
+            listaDePeliculas.forEach(p => {
                 if (searchTerm != "" && p.title.toUpperCase().includes(searchTerm) || searchTerm != "" && p.tagline.toUpperCase().includes(searchTerm) || searchTerm != "" && p.overview.toUpperCase().includes(searchTerm)){
                     let newElement = document.createElement('li')
                     newElement.classList.add("container")
                     newElement.style.opacity = 0
                     newElement.style.transition = "opacity 0.7s ease-in"
                     newElement.innerHTML = convertToHtmlElem(p);
-                    newElement.setAttribute('data-index', index);
                     listContainer.appendChild(newElement)
-                    console.log(index)
+                    newElement.addEventListener("click", () => {
+                      mostrarOffcanvas(p);
+                  });
                 }
-                else p.genres.forEach((element, index) => {
+                else p.genres.forEach(element => {
                   if (element.name.toUpperCase().includes(searchTerm)){
                     let newElement = document.createElement('li')
                     newElement.classList.add("container")
                     newElement.style.opacity = 0
                     newElement.style.transition = "opacity 0.7s ease-in"
                     newElement.innerHTML = convertToHtmlElem(p);
-                    newElement.setAttribute('data-index', index);
-                    listContainer.appendChild(newElement)
-                    console.log(index)
+                    listContainer.appendChild(newElement);
+                    newElement.addEventListener("click", () => {
+                      mostrarOffcanvas(p);
+                  });
                   }
                  })
                 if(listContainer.childNodes.length <= 0){
@@ -77,44 +79,32 @@ document.addEventListener("DOMContentLoaded", function(e){
                 },500*i)
               }                     
          }); //Aqui termina el forEach inicial dentro de la funcion showProductList
-      }; //Aqui termina la funcion showProductList
-  
-      listContainer.addEventListener('click', (event) => {
-        if (event.target.tagName === 'li') {
-          // Obtiene el índice almacenado en el atributo data-index del elemento <li>
-          const selectedIndex = event.target.getAttribute('data-index');
-          
-          // Asegúrate de que el índice sea válido
-          if (selectedIndex !== null) {
-            // Muestra el offcanvas con los detalles de la película seleccionada
-            const peliculaSeleccionada = currentProductsArray[selectedIndex];
-            let generos = currentProductsArray[selectedIndex].genres;
-            const nombresDeGeneros = generos.map((genero) => genero.name);
-            const fechaLanzamiento = currentProductsArray[selectedIndex].release_date;
-            const anioLanzamiento = new Date(fechaLanzamiento).getFullYear();
-            mostrarOffcanvas(peliculaSeleccionada, nombresDeGeneros, anioLanzamiento);
-          }
-        }
-      });
-      
+      }; //Aqui termina la funcion mostrarDetallesPelicula     
   }); //Aqui termina el getJsonData
 
    // Función para mostrar el offcanvas con los detalles de la película
-   function mostrarOffcanvas(pelicula, nomGeneros, anio) {
-    // Obtén el elemento del offcanvas
-     const offcanvas = document.getElementById('offcanvas');
-  
-     // Establece la visibilidad del offcanvas utilizando Bootstrap y JavaScript
-     if (offcanvas) {
-       // Muestra el offcanvas
-       const bsOffcanvas = new bootstrap.Offcanvas(offcanvas);
-       bsOffcanvas.show();
-       
-       // Actualiza el contenido del offcanvas
-       const offcanvasBody = offcanvas.querySelector('.offcanvas-body');
-       offcanvasBody.innerHTML = contenedorSuperior(pelicula, nomGeneros, anio);
-     }
-  };
+   function mostrarOffcanvas(pelicula) {
+    const generos = pelicula.genres.map((genre) => genre.name).join(" - ");
+    const tituloPelicula = document.getElementById("tituloPelicula");
+    const descripcionPelicula = document.getElementById("descripcionPelicula");
+    const nombreGeneros = document.getElementById("nombreGeneros");
+    const anioPelicula = document.getElementById("anioPelicula");
+    const runtimePelicula = document.getElementById("runtimePelicula");
+    const presupuestoPelicula = document.getElementById("presupuestoPelicula");
+    const ganaciaPelicula = document.getElementById("ganaciaPelicula");
+    tituloPelicula.textContent = pelicula.title;
+    descripcionPelicula.textContent = pelicula.overview;
+    nombreGeneros.textContent = generos;
+    const fechaLanzamiento = pelicula.release_date;
+    const anioLanzamiento = new Date(fechaLanzamiento).getFullYear();
+    anioPelicula.textContent = anioLanzamiento;
+    runtimePelicula.textContent = pelicula.runtime + " min";
+    presupuestoPelicula .textContent = "$" + pelicula.budget;
+    ganaciaPelicula.textContent = "$" + pelicula.revenue;
+    const offcanvas = new bootstrap.Offcanvas(document.getElementById("offcanvas"));
+    offcanvas.show(); 
+  }; //Aqui termina la funcion que muestra el offcanvas
+
    //Obtiene la cantidad de estrellas de un comentario desde la API
    function getEstrellasHTML(puntos){
     let puntaje = puntos/2;
@@ -140,32 +130,6 @@ document.addEventListener("DOMContentLoaded", function(e){
       </div>
     ` 
   }
-
-//Plantilla Offcanvas con menú despegable
-  function contenedorSuperior(p, nameGenres, anio) {
-     
-    return `<div class="offcanvas offcanvas-start show" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
-     <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasLabel">${p.title}</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" id="close-offcanvas"></button>
-    </div>
-    <div class="offcanvas-body">
-    ${p.overview}
-    </div>
-    <div id="generos">${nameGenres[0]} - ${nameGenres[1]} - ${nameGenres[2]}</div>
-    <div class="dropdown mt-3">
-    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-      More
-    </button>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item">Año: ${anio}</a></li>
-      <li><a class="dropdown-item">Runtime: ${p.runtime}</a></li>
-      <li><a class="dropdown-item">Budget: ${p.budget}</a></li>
-      <li><a class="dropdown-item">Revenue: ${p.revenue}</a></li>
-    </ul>
-    </div>
-    </div>`
-};
 
 });// Aqui termina el DOMContentLoaded
 
